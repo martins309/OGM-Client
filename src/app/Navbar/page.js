75d88741f1845937cdb0adbc3e8d1bc6f3f0aa6f
Andrew Martins <andrew_omartins@yahoo.com>
2024-02-22 00:23:29 -0500

    displayed the nav bar, and not to style it properly

--- src/app/Navbar/page.js
+++ src/app/Navbar/page.js
@@ -1,11 +1,17 @@
+import { menuData } from "./menuData"
+
 export default function NavBar () {
     return (
-        <nav>
+        <nav className="navbar">
+            <ul className="menu">
             {menuData.map((menu, index) => {
-                return(
-                    <MenuItem items={menu} key={index}/>
-                )
+               return (
+                <li className="menu-items" key={index}>
+                    <a href={menu.title}>{menu.title}</a>
+                </li>
+               )
             })}
+            </ul>
         </nav>
     )
 }
