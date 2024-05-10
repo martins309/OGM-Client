44bf8cb3a0cfe14487bed512328051ff3c75750f
Andrew Martins Jr. <75143566+martins309@users.noreply.github.com>
2024-05-10 14:34:51 +0000

    fixing the styling

--- src/app/Navbar/layout.js
+++ src/app/Navbar/layout.js
@@ -1,6 +1,7 @@
 import NavBar from "./page"
+import styles from "./nav.module.css"
 export default function navLayout({ children }){
     return (
-        <NavBar />
+        <NavBar className={styles.navbar}/>
     )
 }
