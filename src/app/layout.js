a4297471ea5b9e6847af91c9399a63bef778b35a
Andrew Martins <andrew_omartins@yahoo.com>
2024-07-24 19:27:02 -0400

    changed the layout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import NavBar from "./Navbar/page";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body 
+      className={inter.className}>
+        <nav>
+          <NavBar />
+        </nav>
+        {children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import NavBar from "./Navbar/page";
 
 export default function Home() {
   return (
-    <main>
-      <NavBar />
-    </main>
+  <div>
+    <h1>this is the home page</h1>
+  </div>
   );
 }
